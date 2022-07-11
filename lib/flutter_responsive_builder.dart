64f58f29fd6f2a9e3ad5303d45708c2b32e4e72c
Ribesh Basnet <90415424+ribesh-code@users.noreply.github.com>
2022-07-11 15:11:00 +0545

    Copied reponsive_builder from flutter skeleton.

---
+++ lib/flutter_responsive_builder.dart
@@ -0,0 +1,7 @@
+library flutter_responsive_builder;
+
+export 'src/device_type.dart';
+export 'src/size_config.dart';
+export 'src/size_info.dart';
+export 'src/helpers.dart';
+export 'src/responsive_builder.dart';
